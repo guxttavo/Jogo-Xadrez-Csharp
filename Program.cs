a9ed9218e3a6b58df8ce1ac8034c99ae304b4992
guxttavo <gustavowbarbosas@gmail.com>
2023-04-03 10:05:12 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using jogo_xadrez.Tabuleiro;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Posicao P = new Posicao(3, 4);
+
+    System.Console.WriteLine("Posicao: " + P);
+  }
+}
---
+++ Tabuleiro/Posicao.cs
@@ -0,0 +1,25 @@
+namespace jogo_xadrez.Tabuleiro
+{
+  public class Posicao
+  {
+    public int linha { get; set; }
+    public int coluna { get; set; }
+    public Posicao(int linha, int coluna)
+    {
+      this.linha = linha;
+      this.coluna = coluna;
+    }
+    //o ToString() é usado para retornar uma representação em formato
+    //string do objeto.
+
+    //A classe posição está sobrescrevendo o método "ToString()" da classe base 'Object
+    //Sem o uso do Override a implementação personalizada não seria aplicada e o comportamento
+    //padrão do método "ToString()" na classe base seria usado em vez disso.
+    public override string ToString()
+    {
+      return linha
+          + ","
+          + coluna;
+    }
+  }
+}
