4f1a614e4a248a43bae5486bc3a6158b51dc4905
guxttavo <gustavowbarbosas@gmail.com>
2023-04-10 11:00:29 -0300

    movimentacao torre

--- Xadrez/Rei.cs
+++ Xadrez/Rei.cs
@@ -4,13 +4,6 @@ namespace xadrez
 {
   class Rei : Peca
   {
-    /*
-      -Base é responsável por mandar os parâmetros da classe derivada para classe base
-      -Isso é importante pois está garantindo que o construtor da classe base seja inicializado
-       primeiro que o construtor da classe derivada
-      -Se a classe base não for inicializada primeiro, a classse derivada pode acbar em um estado
-       inconsistente ou com membros não inicializados
-    */
     public Rei(Tabuleiro tabuleiro, Cor cor) : base(tabuleiro, cor)
     {
     }
